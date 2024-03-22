af88f448c91284448cbf8f9a899344de18c0254a
mofizul21 <mofizul21@gmail.com>
2024-03-22 11:39:49 +0600

    Assignemt 1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import StudentTable from "./components/StudentTable"
+
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <StudentTable />
+      <Footer />
+    </>
+  )
+}
+
+export default App
